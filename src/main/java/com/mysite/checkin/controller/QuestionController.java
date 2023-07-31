adc62119db8088c3989f11962e8594edae77ecb4
jihwan98 <jihwan98.dev@gmail.com>
2023-07-31 10:59:11 +0900

    질문 등록하기 validation

--- src/main/java/com/mysite/checkin/controller/QuestionController.java
+++ src/main/java/com/mysite/checkin/controller/QuestionController.java
@@ -1,11 +1,14 @@
 package com.mysite.checkin.controller;
 
 import com.mysite.checkin.domain.Question;
+import com.mysite.checkin.form.QuestionForm;
 import com.mysite.checkin.repository.QuestionRepository;
 import com.mysite.checkin.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,13 +34,16 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form.html";
     }
 
     @PostMapping("/create")
-    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
-        this.questionService.create(subject, content);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
     }
 }
